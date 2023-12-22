54fe76208426c571a8d846b696e26b9f3a5b4505
Igor <igor.sorokoletov19@yandex.ru>
2023-12-22 16:28:09 +0300

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 5000;
+        int milesCost = 20;
+        int bonuses;
+
+        bonuses = ticket / milesCost;
+        System.out.println("Количество бонусов - " + bonuses);
+    }
+}
